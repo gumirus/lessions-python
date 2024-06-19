ec4fca652e318685804623978a10cc194c2c150b
Ruslan Gumirov <gumirus29@gmail.com>
2024-06-19 15:47:30 +0500

    add...

--- Модуль#2/домашнее задание/zadanie_4.py
+++ Модуль#2/домашнее задание/zadanie_4.py
@@ -45,3 +45,221 @@ cat1 = Cat("Мурка")
 
 dog1.make_sound()
 cat1.make_sound()
+
+# import tkinter as tk
+# from tkinter import messagebox
+# # import simpleaudio as sa
+
+# class Animal:
+#     def __init__(self, name, species):
+#         self.name = name
+#         self.species = species
+
+#     def make_sound(self):
+#         pass  # Абстрактный метод, будет переопределён в подклассах
+
+
+# class Dog(Animal):
+#     def __init__(self, name):
+#         super().__init__(name, 'Dog')
+
+#     def make_sound(self):
+#         wave_obj = sa.WaveObject.from_wave_file('dog.wav')
+#         wave_obj.play()
+
+
+# class Cat(Animal):
+#     def __init__(self, name):
+#         super().__init__(name, 'Cat')
+
+#     def make_sound(self):
+#         wave_obj = sa.WaveObject.from_wave_file('cat.wav')
+#         wave_obj.play()
+
+
+# class App(tk.Tk):
+#     def __init__(self):
+#         super().__init__()
+#         self.title('Animal Sounds')
+#         self.geometry('300x200')
+
+#         self.label = tk.Label(self, text='Click the buttons to hear animal sounds!')
+#         self.label.pack(pady=20)
+
+#         self.dog_button = tk.Button(self, text='Dog Sound', command=self.play_dog_sound)
+#         self.dog_button.pack(pady=10)
+
+#         self.cat_button = tk.Button(self, text='Cat Sound', command=self.play_cat_sound)
+#         self.cat_button.pack(pady=10)
+
+#     def play_dog_sound(self):
+#         dog = Dog('Buddy')
+#         dog.make_sound()
+
+#     def play_cat_sound(self):
+#         cat = Cat('Whiskers')
+#         cat.make_sound()
+
+
+# # Создание и запуск одного экземпляра приложения
+# if __name__ == '__main__':
+#     app = App()
+#     app.mainloop()
+
+
+# import tkinter as tk
+# from tkinter import messagebox
+# import pygame
+
+# class Animal:
+#     def __init__(self, name, species):
+#         self.name = name
+#         self.species = species
+
+#     def make_sound(self):
+#         pass  # Abstract method, to be overridden in subclasses
+
+
+# class Dog(Animal):
+#     def __init__(self, name):
+#         super().__init__(name, 'Dog')
+
+#     def make_sound(self):
+#         pygame.mixer.init()
+#         sound = pygame.mixer.Sound('dog.wav')
+#         sound.play()
+
+
+# class Cat(Animal):
+#     def __init__(self, name):
+#         super().__init__(name, 'Cat')
+
+#     def make_sound(self):
+#         pygame.mixer.init()
+#         sound = pygame.mixer.Sound('cat.wav')
+#         sound.play()
+
+
+# class App(tk.Tk):
+#     def __init__(self):
+#         super().__init__()
+#         self.title('Animal Sounds')
+#         self.geometry('300x200')
+
+#         self.label = tk.Label(self, text='Click the buttons to hear animal sounds!')
+#         self.label.pack(pady=20)
+
+#         self.dog_button = tk.Button(self, text='Dog Sound', command=self.play_dog_sound)
+#         self.dog_button.pack(pady=10)
+
+#         self.cat_button = tk.Button(self, text='Cat Sound', command=self.play_cat_sound)
+#         self.cat_button.pack(pady=10)
+
+#     def play_dog_sound(self):
+#         dog = Dog('Buddy')
+#         dog.make_sound()
+
+#     def play_cat_sound(self):
+#         cat = Cat('Whiskers')
+#         cat.make_sound()
+
+
+# # Create and run a single instance of the application
+# if __name__ == '__main__':
+#     app = App()
+#     app.mainloop()
+
+
+# import tkinter as tk
+# from tkinter import messagebox
+# import pygame
+# import os
+
+# class Animal:
+#     def __init__(self, name, species):
+#         self.name = name
+#         self.species = species
+
+#     def make_sound(self):
+#         pass  # Абстрактный метод, должен быть переопределен в подклассах
+
+# class Dog(Animal):
+#     def __init__(self, name):
+#         super().__init__(name, 'Dog')
+
+#     def make_sound(self):
+#         pygame.mixer.init()
+#         sound_file = 'dog.mp3'
+#         if os.path.exists(sound_file):
+#             pygame.mixer.music.load(sound_file)
+#             pygame.mixer.music.play()
+#         else:
+#             print(f"Файл '{sound_file}' не найден!")
+
+# class Cat(Animal):
+#     def __init__(self, name):
+#         super().__init__(name, 'Cat')
+
+#     def make_sound(self):
+#         pygame.mixer.init()
+#         sound_file = 'cat.mp3'
+#         if os.path.exists(sound_file):
+#             pygame.mixer.music.load(sound_file)
+#             pygame.mixer.music.play()
+#         else:
+#             print(f"Файл '{sound_file}' не найден!")
+
+# class App(tk.Tk):
+#     def __init__(self):
+#         super().__init__()
+#         self.title('Animal Sounds')
+#         self.geometry('300x200')
+
+#         self.label = tk.Label(self, text='Нажмите на кнопки, чтобы услышать звуки животных!')
+#         self.label.pack(pady=20)
+
+#         self.dog_button = tk.Button(self, text='Звук собаки', command=self.play_dog_sound)
+#         self.dog_button.pack(pady=10)
+
+#         self.cat_button = tk.Button(self, text='Звук кошки', command=self.play_cat_sound)
+#         self.cat_button.pack(pady=10)
+
+#     def play_dog_sound(self):
+#         dog = Dog('Buddy')
+#         dog.make_sound()
+
+#     def play_cat_sound(self):
+#         cat = Cat('Whiskers')
+#         cat.make_sound()
+
+# # Создание и запуск единственного экземпляра приложения
+# if __name__ == '__main__':
+#     app = App()
+#     app.mainloop()
+
+# class Dog(Animal):
+#     def __init__(self, name):
+#         super().__init__(name, 'Dog')
+
+#     def make_sound(self):
+#         pygame.mixer.init()
+#         sound_file = 'C:/Users/agori/.vscode/projects/lessions-python/dog.mp3'
+#         if os.path.exists(sound_file):
+#             pygame.mixer.music.load(sound_file)
+#             pygame.mixer.music.play()
+#         else:
+#             print(f"Файл '{sound_file}' не найден!")
+
+# class Cat(Animal):
+#     def __init__(self, name):
+#         super().__init__(name, 'Cat')
+
+#     def make_sound(self):
+#         pygame.mixer.init()
+#         sound_file = 'C:/Users/agori/.vscode/projects/lessions-python/cat.mp3'
+#         if os.path.exists(sound_file):
+#             pygame.mixer.music.load(sound_file)
+#             pygame.mixer.music.play()
+#         else:
+#             print(f"Файл '{sound_file}' не найден!")
+
